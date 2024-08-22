5661d5fe32bdcefb96d92e48c6a2ccc285241f7f
nicolasplanas <nicolasplanasgomes27@gmail.com>
2024-08-21 22:29:47 -0300

    Interação com js

---
+++ js/open-and-closed.js
@@ -0,0 +1,45 @@
+function openMenu() {
+    document.getElementById('menu').style.display = 'flex'
+    document.getElementById('header').style.display = 'none'
+}
+
+function closeMenu() {
+    document.getElementById('menu').style.display = 'none'
+    document.getElementById('header').style.display = 'flex'
+}
+
+function openMenu2() {
+    document.getElementById('menu').style.display = 'flex'
+}
+
+function closeMenu2() {
+    document.getElementById('menu').style.display = 'none'
+}
+
+function openSearch() {
+    document.getElementById('search').style.display = 'flex'
+    document.getElementById('header').style.display = 'none'
+}
+
+function closeSearch() {
+    document.getElementById('search').style.display = 'none'
+    document.getElementById('header').style.display = 'flex'
+}
+
+function consultoria() {
+    document.getElementById('tab-consultoria').style.display = 'flex'
+    document.getElementById('tab-serviço').style.display = 'none'
+}
+
+function serviço() {
+    document.getElementById('tab-serviço').style.display = 'flex'
+    document.getElementById('tab-consultoria').style.display = 'none'
+}
+
+function openModal(){
+    document.getElementById('modal').style.display = 'flex'
+}
+  
+function closeModal(){
+    document.getElementById('modal').style.display = 'none'
+}
